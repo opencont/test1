774d08d5dad7bd6a7258476c7912a6f38b6b231d
arpitgupta2774 <64141687+arpitgupta2774@users.noreply.github.com>
2020-10-15 19:42:30 +0530

    Add files via upload

---
+++ SlopeStability/constants.py
@@ -0,0 +1,31 @@
+import array
+
+S = 10
+S1 = 8
+
+a_min =  47.25 
+a_max =  90.00 
+
+b_min =  75.00
+b_max =  125.00
+
+R_min =  20.00
+R_max =  75.00
+
+slopeTop_x = 25.00 
+slopeTop_y = 70.00 
+
+slopeBottm_x = 75.30 
+slopeBottm_y = 45.00 
+
+slopeToe_x = 0.5 
+slopeToe_y = 70.00 
+
+Slope_BaseToe_x =  85.00  
+Slope_BaseToe_y = 45.00 
+
+UnitWight = 20.0 
+cohesion = array.array('d', [10,10,10,10,10,10,10,10,10])
+phi = array.array('d', [26.6,26.6,26.6,26.6,26.6,26.6,26.6,26.6,26.6]) 
+Pore_pressure = array.array('d', [0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0])
+
